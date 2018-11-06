f3d91cc237248d8f82bdac3bd0322af57058cf63
mingquan_liu@163.com <lmq5083468163>
2018-11-06 19:30:13 +0800

    大写

---
+++ src/main/java/com/sosbuger/custom/Custom.java
@@ -0,0 +1,9 @@
+package  com.sosbuger.custom;
+
+public interface Custom {
+
+    public final static String BAIDU = "http://www.baidu.com/";
+    public final static String GETIP = "http://members.3322.org/dyndns/getip/";
+
+
+}
